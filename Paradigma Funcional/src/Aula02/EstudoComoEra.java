1188f1598d499606164e6de19bac5a36e89f4daf
casdea <casdea@gmail.com>
2021-08-18 20:50:13 -0300

    Utilizando composicao de funcoes do paradigma funcional e o paradigma imperativo

---
+++ Paradigma Funcional/src/Aula02/EstudoComoEra.java
@@ -0,0 +1,23 @@
+package Aula02;
+
+import java.lang.reflect.Method;
+
+public class Estudo {
+    public static void main(String[] args) {
+
+    }
+
+    /*
+
+    nao é possivel passar o metodo somar como prametro
+    public static void calcular(somar(x,y)) {
+
+    }
+    */
+
+    public static int somar(int x, int y)
+    {
+
+    }
+}
+
